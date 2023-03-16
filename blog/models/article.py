4e7a62713d4b8ea097e91c9d23324ce5f2e689f1
Alex <gofavalon@gmail.com>
2023-03-16 20:50:01 +0400

    Revert "Authors+Articles"
    This reverts commit 00d462cef74a9f1fccfaf1a2fe04c638f96a96fe.

--- blog/models/article.py
+++ blog/models/article.py
@@ -1,19 +1,10 @@
-from datetime import datetime
-
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean
 from blog.models.databases import db
 
 
 class Article(db.Model):
     __tablename__ = 'articles'
 
-    id = db.Column(db.Integer, primary_key=True)
-    article_id = db.Column(db.Integer, ForeignKey('authors.id'), nullable=False)
-    title = db.Column(db.String(255))
-    text = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    author = relationship('Author', back_populates='articles')
+    title = Column(String(255))
+    text = Column(String)
 
