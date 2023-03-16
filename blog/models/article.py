00d462cef74a9f1fccfaf1a2fe04c638f96a96fe
Alex <gofavalon@gmail.com>
2023-03-16 20:50:01 +0400

    Authors+Articles

--- blog/models/article.py
+++ blog/models/article.py
@@ -1,10 +1,19 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from blog.models.databases import db
 
 
 class Article(db.Model):
     __tablename__ = 'articles'
 
-    title = Column(String(255))
-    text = Column(String)
+    id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.Integer, ForeignKey('authors.id'), nullable=False)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    author = relationship('Author', back_populates='articles')
 
